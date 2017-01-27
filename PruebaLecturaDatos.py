780d6027f75eaaa33debd694dd7308b89c4d01c6
Bryan Reinoso Cevallos <bryanreinosocevallos@MacBook-Air-de-Bryan.local>
2017-01-27 18:15:01 +0100

    Testing Mean Normalization
    Test wit mean normalization

--- PruebaLecturaDatos.py
+++ PruebaLecturaDatos.py
@@ -22,6 +22,19 @@ def processFile(str):
         x.append(temp1)
         y.append(temp2)
     return x,y
+def normalization(x):
+    norm=[]
+    maximum=[]
+    minumum=[]
+    for i in range(len(x[0])):
+        maximum.append(max(x[:][i]))
+        minimum.append(min(x[:][i]))
+    for i in x:
+        temp=[]
+        for j in range(len(i)):
+            temp.append((i[j]-minimum[j])/(maximum[j]-minimum[j]))
+        norm.append(temp)
+    return norm
 
 if __name__ == "__main__":
     inputs=[]
@@ -35,11 +48,6 @@ if __name__ == "__main__":
             [temp1,temp2]=processFile(SWIT)
         elif i == VADATA:
             [temp1,temp2]=processFile(VADATA)
-        inputs.append(temp1)
-        ouputs.append(temp2)
-    lenT=int(len(inputs)*0.7)
-    inputsTr=inputs[:lenT]
-    inputsTe=inputs[lenT:]
-    ouputsTr=ouputs[:lenT]
-    ouputsTe=ouputs[lenT:]
-    
+        inputs.extend(temp1)
+        ouputs.extend(temp2)
+    inputsNorm=normalization(inputs)
